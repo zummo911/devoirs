5778e4fd7218dec6871d34a611dd1afb9ba4c4d6
zummo911 <145589598+zummo911@users.noreply.github.com>
2023-09-27 19:46:37 +0200

    Add files via upload

---
+++ variablesandstuff 27.09.py
@@ -0,0 +1,11 @@
+numb = 89
+swimming_dot = 31.02
+animal = "dog"
+my_favourite_variables = [89,31.02,"dog"]
+print (type(numb), type(swimming_dot), type(animal), type(my_favourite_variables))
+numb = str(numb)
+swimming_dot = int(swimming_dot)
+animal = list(animal)
+my_favourite_variables = str(my_favourite_variables)
+print (numb, swimming_dot, animal, my_favourite_variables)
+
